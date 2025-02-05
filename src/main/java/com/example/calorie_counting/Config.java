f339f3cb9c64cffd4deacd2cc551accdf4ecb08a
Mox <tenmaken246@gmail.com>
2025-02-06 01:06:16 +0300

    добавила sql

--- src/main/java/com/example/calorie_counting/Config.java
+++ src/main/java/com/example/calorie_counting/Config.java
@@ -4,6 +4,6 @@ public class Config {
     protected String dbHost = "localhost";
     protected String dbPort = "3306";
     protected String dbUser = "root";
-    protected String dbPass = "171015246Mox@";
+    protected String dbPass = "171015246mox";
     protected String dbName = "calorii_projektt";
 }
